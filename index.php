04de47a2dddbb58695dff768e2841710dee4767d
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-11-21 20:10:52 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if($method == "POST"){
 
       $response = new \stdClass();
       $response->speech = $speech;
-      $response->displayText = $speech;
+      $response->fulfillmentText = $speech;
       $response->source = "webhook";
       echo json_encode($response);
 

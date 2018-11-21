8c9bdcf57c63f13dd3fbef7365baea44eb1f2229
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-11-21 18:20:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST"){
+      $requestBody = file_get_contents('php://imput');
+      $json = json_decode($requestBody);
+
+      $text = $json->result->parameters->text;
+
+      switch ($text) {
+        case 'hola':
+          $speech = "Hola! Un placer conocerte!";
+          break;
+        case 'chau':
+          $speech = "Hasta pronto!";
+          break;
+
+        default:
+          $speech = "No entendí.¿Me lo repetirías?";
+          break;
+      }
+
+      $response = new \stdClass();
+      $response->speech = "";
+      $response->displayText = "";
+      $response->source = "webhook";
+      echo json_encode($response);
+
+}else{
+     echo "Method not allowed";
+}
+
+
+
+
+
+
+
+
+ ?>

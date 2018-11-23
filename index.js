78d1ab65920f38ef8f0a5b1af96c4082a9a8ab11
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-11-23 12:08:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,86 @@
-const functions = require('firebase-functions')
-const {dialogflow} =require('actions-on-google')
+'use strict';
 
-const WELCOME_INTENT = 'Default Welcome Intent'
-const FALLBACK_INTENT= 'Default Fallback Intent'
-const NEED_QUOTE_INTENT= 'Need Quote'
-const QUOTE_TYPE_ENTITY= 'TypeOfQuote'
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const restService = express();
 
-const app=dialogflow()
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+restService.use(bodyParser.json());
 
-app.intent(WELCOME_INTENT, (conv)=> {
-    conv.ask("Hola Santiago! Te gustaria escuchar una frase?")
- })
+restService.post('/echo', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    //var speech = "Hola Jorge";
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
 
-app.intent(FALLBACK_INTENT, (conv)=> {
-    conv.ask("No entendí nada")
- })
+restService.post('/slack-test', function(req, res) {
 
- app.intent(NEED_QUOTE_INTENT, (conv)=> {
-    const type= conv.parameters[QUOTE_TYPE_ENTITY].toLowerCase();
-    switch (type){
-        case "felicidad":
-             conv.ask("Aquellos que quieren cantar siempre encuentran una canción")
-            break;
-        case "amor":
-             conv.ask("No hay amor imposible en esta vida, lo que sí existe son las personas que desisten justo antes de alcanzar el verdadero amor")
-            break;
-        case "amistad":
-            conv.ask("Lo amigos son la familia que se escoge")
-            break;
-        case "inspiracion":
-            conv.ask("Solo los que corren el riesgo de avanzar, pueden saber hasta dónde pueden llegar")
-            break;
-        default:
-            conv.ask("El destino mezcla las cartas, y nosotros las jugamos")
+    var slack_message = {
+        "text": "Details of JIRA board for Browse and Commerce",
+        "attachments": [{
+            "title": "JIRA Board",
+            "title_link": "http://www.google.com",
+            "color": "#36a64f",
+
+            "fields": [{
+                "title": "Epic Count",
+                "value": "50",
+                "short": "false"
+            }, {
+                "title": "Story Count",
+                "value": "40",
+                "short": "false"
+            }],
+
+            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+        }, {
+            "title": "Story status count",
+            "title_link": "http://www.google.com",
+            "color": "#f49e42",
+
+            "fields": [{
+                "title": "Not started",
+                "value": "50",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }]
+        }]
     }
+    return res.json({
+        speech: "speech",
+        displayText: "speech",
+        source: 'webhook-echo-sample',
+        data: {
+            "slack": slack_message
+        }
+    });
+});
+
+
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
+
 
 
 

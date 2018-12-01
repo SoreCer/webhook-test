734ab69b2be610cfab5367bdfd641b5504ba1302
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-12-01 16:46:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(bodyParser.json());
 restService.post('/quote', function(req, res) {
      //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     //var speech = "Hola santi";
-    var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
+    var type =req.body.result.parameters.TypeOfQuote.toLowerCase();
     switch (type){
         case "felicidad":
              var speech ="Aquellos que quieren cantar siempre encuentran una canción";

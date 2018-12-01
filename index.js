99e842a70ebf7d281cbdc1f50530453b515ee95d
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-12-01 16:40:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+''use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     //var speech = "Hola santi";
     /*var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
     switch (type){
@@ -31,7 +31,6 @@ restService.post('/echo', function(req, res) {
         default:
             var speech ="El destino mezcla las cartas, y nosotros las jugamos";
     }*/
-
     return res.json({
         speech: speech,
         displayText: speech,
@@ -98,10 +97,3 @@ restService.post('/slack-test', function(req, res) {
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
-
-
-
-
- })
-
- exports.dialogflowFirebaseFulfillment= functions.https.onRequest(app)

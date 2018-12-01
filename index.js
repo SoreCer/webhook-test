072622a6e15a62450fa7b9b177a155e4386a39bc
SoreCer <45244691+SoreCer@users.noreply.github.com>
2018-12-01 16:43:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     //var speech = "Hola santi";
-    /*var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
+    var type = req.body.result && req.body.result.parameters && req.body.result.parameters.TypeOfQuote;
     switch (type){
         case "felicidad":
              var speech ="Aquellos que quieren cantar siempre encuentran una canción";
@@ -30,7 +30,7 @@ restService.post('/echo', function(req, res) {
             break;
         default:
             var speech ="El destino mezcla las cartas, y nosotros las jugamos";
-    }*/
+    }
     return res.json({
         speech: speech,
         displayText: speech,
